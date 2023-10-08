2f25244e4178163f1c8b569ab7d2910a44be47cd
Vision Onyeaku <chizurumokeonyeaku@gmail.com>
2023-10-08 18:14:54 +0100

    updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as elementsIntersect } from './elementsIntersect'
-export { DragSelection } from './DragSelection'
+export { DragSelection, SelectionBox } from './DragSelection'
 export { default as useDragSelection, SELECTABLE_ITEM_CLASS, DISABLE_SELECTION_CLASS } from './useDragSelection'
